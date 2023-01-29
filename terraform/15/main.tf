8c3c02986e41e0f37fb3fd091534e6c6e48dc571
ArtemSalnikov <79057917712@yandex.ru>
2023-01-29 18:34:31 +0300

    HW 15.1

--- terraform/15/main.tf
+++ terraform/15/main.tf
@@ -92,24 +92,3 @@ resource "yandex_compute_instance" "vm_private" {
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
 }
-
-
-
-
-
-#output "internal_ip_address_vm_1" {
-#  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
-#}
-
-#output "internal_ip_address_vm_2" {
-#  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
-#}
-
-
-#output "external_ip_address_vm_1" {
-#  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-#}
-
-#output "external_ip_address_vm_2" {
-#  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
-#}
