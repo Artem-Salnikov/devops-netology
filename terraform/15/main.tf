afdfcfb4362404249e7a09cad3bf8295523b78da
ArtemSalnikov <79057917712@yandex.ru>
2023-01-29 18:25:46 +0300

    HW 15.1

--- terraform/15/main.tf
+++ terraform/15/main.tf
@@ -30,7 +30,7 @@ resource "yandex_vpc_subnet" "private" {
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.20.0/24"]
-  #route_table_id = yandex_vpc_route_table.tonat.id
+  route_table_id = yandex_vpc_route_table.tonat.id
 }
 
 resource "yandex_vpc_route_table" "tonat" {
