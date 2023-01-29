8cba5fb326f37ffec6cef6cf0d4d07a7b231d361
ArtemSalnikov <79057917712@yandex.ru>
2023-01-29 18:23:51 +0300

    HW 15.1

---
+++ terraform/15/main.tf
@@ -0,0 +1,115 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  service_account_key_file = "key.json"
+  cloud_id = var.YC_CLOUD_ID
+  folder_id = var.YC_FOLDER_ID
+  zone = "ru-central1-a"
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "public" {
+  name           = "public"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_vpc_subnet" "private" {
+  name           = "private"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.20.0/24"]
+  #route_table_id = yandex_vpc_route_table.tonat.id
+}
+
+resource "yandex_vpc_route_table" "tonat" {
+  name = "tonat"
+  network_id = yandex_vpc_network.network-1.id
+
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    next_hop_address = yandex_compute_instance.vm_public.network_interface.0.ip_address
+  }
+}
+
+resource "yandex_compute_instance" "vm_public" {
+  name = "nat-instance"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80mrhj8fl2oe87o4e1"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.public.id
+    nat       = true
+    ip_address = "192.168.10.254"
+    
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm_private" {
+  name = "ubuntu"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8ingbofbh3j5h7i8ll"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.private.id
+    nat       = false    
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+
+
+
+
+#output "internal_ip_address_vm_1" {
+#  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+#}
+
+#output "internal_ip_address_vm_2" {
+#  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+#}
+
+
+#output "external_ip_address_vm_1" {
+#  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+#}
+
+#output "external_ip_address_vm_2" {
+#  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+#}
---
+++ terraform/15/variables.tf
@@ -0,0 +1,7 @@
+variable "YC_CLOUD_ID" {
+  default = ""
+}
+
+variable "YC_FOLDER_ID" {
+  default = ""
+}
